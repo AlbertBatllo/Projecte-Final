7e33de265c2eb1e8eb8bf095fa32ea191308fa6b
AlbertBatllo <100155905+AlbertBatllo@users.noreply.github.com>
2022-06-06 14:03:25 +0200

    Update main.cpp

--- Projecte final/src/main.cpp
+++ Projecte final/src/main.cpp
@@ -2,47 +2,35 @@
 // Lector de targetas
 #include <SPI.h>
 #include <MFRC522.h>
-#define RST_PIN 17 //Pin 9 para el reset del RC522
-#define SS_PIN 4 //Pin 10 para el SS (SDA) del RC522
+#define RST_PIN 17 
+#define SS_PIN 4
  
 // Wifi
-String ssid =     "*******";
-String password = "*******";
+#include <WiFi.h>
+const char* ssid = "iPhone de Sonia";
+const char* password = "sonia123";
+
+// Pagina web
+#include <WebServer.h>
+WebServer server(80);
  
 // Zumbador
 #include <EasyBuzzer.h>
  
-// Display
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_I2CDevice.h"
-#include <Adafruit_NeoPixel.h>
- 
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64
-#define I2Cdisplay_SDA 22
-#define I2Cdisplay_SCL 16
-TwoWire I2Cdisplay = TwoWire(1);
 // Variables
 int ledblanc = 12;
 int ledverd = 13;
 int ledvermell = 14;
 bool correcte = true;
-bool si = true;
 int zumbador = 2;
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2Cdisplay, -1);
  
 // Funciones
-void PICAR (); //Picar al timbre
-void PASA (); //Esta sera la funcion que "abrira la puerta"
-void COMPARAR(); //Compara el pin de la targeta con los que tiene guardados
-void DENEGADO (); //Funcion que se activa cuando la targeta es incorrecta
-void PANTALLA (); //Funcion que hace el display
-//void PANTALLA(bool si);
-void PAGWEB ();
+void PICAR (); // Picar al timbre
+void PASA (); // Esta sera la funcion que "habrira la puerta"
+void COMPARAR();
+void DENEGADO ();
+void handle_root (void); // Pagina web
 
 void setup() {
   Serial.begin(115200); //Iniciamos la comunicación serial
@@ -57,23 +45,22 @@ void setup() {
   pinMode (15, INPUT_PULLUP); // timbre
   pinMode (2, OUTPUT); // zumbador
   // Zumbador
-  //configuracion vacia (){ // de forma predeterminada, la libreria esta configurada para usar el pin 4, cabiamos de pin llamando a la configurazion
   EasyBuzzer.setPin(zumbador);
-  //Display
-  I2Cdisplay.begin(I2Cdisplay_SDA, I2Cdisplay_SCL, 100000); // inicializamos el display
-  Wire.begin();
-  while (!Serial);
-  Serial.println("\nI2C Scanner");
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-  Serial.println(F("SSD1306 allocation failed"));
-  for(;;); // Don't proceed, loop forever
-  display.display();
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.drawPixel(10, 10, SSD1306_WHITE);
- 
+  // Wifi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.print(".");
+  }
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP()); //Show ESP32 IP on serial
+  // Pagina web
+  server.begin();
+  server.on("/", handle_root);
 }
+
 byte Usuario1[4]= {0x50, 0x60, 0x13, 0x4E} ; //código del usuario 1
+
 void loop() {
   int timbre = digitalRead (15);
   int boto = digitalRead (21);
@@ -84,17 +71,17 @@ void loop() {
   if (boto==LOW){
      PASA();
   }
-  // Revisamos si hay nuevas tarjetas presenfalsetes
-  if ( mfrc522.PICC_IsNewCardPresent()){
-      //Seleccionamos una tarjeta
+  // Revisamos si hay nuevas tarjetas
+  if ( mfrc522.PICC_IsNewCardPresent())
+  {
+      //Seleccionamos la tarjeta
       if ( mfrc522.PICC_ReadCardSerial())
       {
         // Encendemos el led de control (blanco)
         digitalWrite(ledblanc, HIGH);
-        delay(500);               // el deixem ences un moment
+        delay(500);
         digitalWrite(ledblanc, LOW);
-        // Enviamos serialemente su UID
-        Serial.print("Card UID:"); //..........................POSIBLE DISPLAY
+
         COMPARAR();
         if (correcte){
           PASA();
@@ -104,20 +91,21 @@ void loop() {
         }
       }
   }
+  
+}
 // Esta funcion simula un timbre
 void PICAR (){
   EasyBuzzer.beep (1000, 900000); // frequencia en Hz, duracion del pitido en ms
-  //EasyBuzzer.setOnDuration (900000);
   EasyBuzzer.stopBeep();
 }
-// Esta función encendera el led verde y la pagina web...................................................................................................
+// Esta función encendera el led verde y la pagina web
 void PASA (){
   // encendemos led verde
   digitalWrite(ledverd, HIGH);
   delay(500);               // el deixem ences un moment
   digitalWrite(ledverd, LOW);
-  si = true;
-  //PANTALLA(si);
+  Serial.println("Targeta ACEPTADA");
+  server.handleClient(); // pag web
 }
 void COMPARAR(){
   correcte = true;
@@ -128,39 +116,34 @@ void COMPARAR(){
     }
   }
 }
-  // mostramos por pantalla#include <Arduino.h>
- 
-void loop() {
- // put your main code here, to run repeatedly:
-  byte i;
-  Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-  Serial.print(mfrc522.uid.uidByte[i], HEX);
-  // Terminamos la lectura de la tarjeta actual
-  mfrc522.PICC_HaltA();
-}
+
 void DENEGADO (){
 // encendemos led roja
   digitalWrite(ledvermell, HIGH);
   delay(500);
   digitalWrite(ledvermell, LOW);
-  Serial.println();//display.drawPixel(10, 10, SSD1306_WHITE);
+  Serial.println();
   // Terminamos la lectura de la tarjeta actual
   mfrc522.PICC_HaltA();
-  si = false;
- //PANTALLA(si);
+ Serial.println("Targeta DENEGADA");
 }
- 
- void PANTALLA(bool si){
- display.clearDisplay();
- display.drawPixel(10, 10, SSD1306_WHITE);
- display.setTextColor(WHITE);
- display.setTextSize(5);
- display.setCursor(19,5);
- if (si){
-   display.print("Bienvenido");
- }
- else{
-   display.print("Tarjeta incorrecta");
- }
- }
+
+String HTML ="<!DOCTYPE html>\
+<html>\
+  <head>\
+  <meta charset='utf-8' />\
+  <title>PORTAL </title>\
+  </head>\
+  <body>\
+  <center>\
+  <h2> <strong> ENTRADA PORTAL EDIFICIO E </strong> </h2>\
+  <p> <strong>Acaba de entrar por la porteria el Usuario1</strong> </p>\
+  <p> Propietario del 2º 1ª </p>\
+  <br>\
+  </center>\
+  </body>\
+</html>";
+
+void handle_root(){
+server.send(200, "text/html", HTML);
 }
